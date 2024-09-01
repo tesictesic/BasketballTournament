5efc18ec278c73c3ac0be33ad70d4e9b18fe5bef
Djordje Tesic <djordje.tesa@gmail.com>
2024-09-01 12:35:12 +0200

    bonus task

---
+++ BasketballTournamentC#/BusinessLayer/ExhibitionResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasketballTournamentC_.BusinessLayer
+{
+    public class ExhibitionResult
+    {
+        public string Name { get; set; }
+        public List<Match> Matches { get; set; }
+
+    }
+    public class Match
+    {
+        public string Date { get; set; }
+        public string Opponent { get; set; }
+        public string Result { get; set; }
+    }
+}
